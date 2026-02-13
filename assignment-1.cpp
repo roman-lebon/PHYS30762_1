c7efb921e090a6563c507d5eb27b8fdacb97d95f
roman-lebon <roman.lebon@student.manchester.ac.uk>
2026-02-13 19:38:04 +0000

    Final formatting checks and QOL changes

---
+++ assignment-1.cpp
@@ -0,0 +1,126 @@
+// assignment-1.cpp
+// PHYS30762 Assignment 1 – The Bohr Atom
+// Roman le Bon - 11423027
+
+// Include libraries needed for input, output, and strings
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+// Include function before main to calculate the energy, given the four inputs
+double formula(int Z, int n_i, int n_j, char unit) {
+
+    double E = 136000.0 * Z * Z * (1.0/(n_j*n_j) - 1.0/(n_i*n_i)); // Formula provided
+
+    if (unit == 'e') {
+        return E;
+    }
+
+    else if (unit == 'J'){
+        E = E * 1.602176634e-19; // Conversion from Electron Volts to Joules
+        return E;
+    }
+
+    return 0.0;
+  }
+
+// We always include a main function at program startup
+int main() {
+  // Declare variables - no initialisation as user(s) will input values
+  int Z; // Atomic number (integer)
+  int n_i; // Initial energy level (integer)
+  int n_j; // Final energy level (integer)
+  char unit; // unit for output, either J (Joules) or e (Electron Volts) (character)
+
+  char restart = 'y'; // Gives user the option to restart calculation
+
+  while (restart == 'y') {
+
+    // Ask the user to input values for Z, n_i, n_j, unit
+    std::cout << "\nPlease enter values for: Atomic number, initial quantum number, final quantum number, and unit (J or e):"; // Use the output stream std::cout to print a message to ask the user
+    std::cout << "\nEnter these values, seperated by spaces, in the order they appear above: "; // Prompt user on correct input format
+
+    // We must check the formatting and mathematical validity of the user's input
+    // We can use a loop to prompt the user for new inputs until their input is valid
+
+    bool valid_input = false; // Declare and initialise a boolean variable which tracks validity of user input
+
+    // Create a loop that prompts new input until all validity checks are passed
+    while (!valid_input) {
+    
+        // Declare a string to store user input from std:cin in (makes checks easier)
+        std::string input;
+        std::getline(std::cin, input); 
+        std::stringstream input_stream(input); 
+        input_stream >> Z >> n_i >> n_j >> unit; // Extract the values from the input string into the required variables
+
+        // Check the validity of the input formatting
+        if (input_stream.fail()) {
+            std::cout << "Invalid input format. Refer above to the input instructions, and try again: ";
+            continue;
+        }
+
+        std::string additional_characters;
+        if (input_stream >> additional_characters) { // Check if there are any additional characters included after the four that should be input
+            std::cout << "Invalid input format. Too many values provided. Please try again: ";
+        continue;
+        }
+
+        // Check the validity of the atomic number 
+        if (Z < 1) {
+            std::cout << "Invalid atomic number (must be a positive integer). Please try again: ";
+        }
+
+        // Check the validity of the quantum numbers
+        else if (n_i < 1 || n_j < 1) {
+            std::cout << "Invalid initial/final quantum numbers (must be positive integers). Please try again: ";
+        }
+
+        else if (n_i < n_j) { // CHECK IF THIS CAN BE <= OR JUST < WITH LECTURERS
+            std::cout << "Invalid initial/final quantum numbers (initial must be greater than final). Please try again: ";
+        }
+
+        // Check the validity of the units
+        else if (unit != 'J' && unit != 'e') {
+            std::cout << "Invalid unit (must be J or e). Please try again: ";
+        }
+
+        // End loop if all checks passed
+        else {
+            valid_input = true;
+        }
+
+        }
+
+    double E = formula(Z, n_i, n_j, unit); // Call function defined earlier with user input values
+
+    std::cout << "Transition energy = " << E; // Output result
+
+    // Make sure we output result with correct units
+    if (unit == 'e') {
+        std::cout << " eV\n";
+    }
+
+    else {
+        std::cout << " J\n";
+    }
+
+    // Prompt user whether to restart/end the programme
+    std::cout << "Would you like to calculate another value? Please enter (y/n): ";
+    std::cin >> restart;
+    std::cin.ignore(10000, '\n'); // Clear leftover newline so getline() doesn't read an empty line after restart and interpret it as an invalid input 
+
+    
+    // Check user input is valid 
+    while (restart != 'y' && restart != 'n') {
+        std::cout << "Invalid input. Please enter only (y/n): ";
+        std::cin >> restart;
+    }
+
+    }
+
+    return 0;
+}
+
+
