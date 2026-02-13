ffc1935bf872fffba12ee247ae5c408c83284edf
roman-lebon <roman.lebon@student.manchester.ac.uk>
2026-02-13 11:42:54 +0000

    Added y/n functionality to restart/end programme

--- proj1.cpp
+++ proj1.cpp
@@ -26,72 +26,86 @@ double formula(int Z, int n_i, int n_j, char unit) {
 // We always include a main function at program startup
 int main() {
   // Declare variables - no initialisation as user(s) will input values
-  int Z; // atomic number (integer)
-  int n_i; // initial energy level (integer)
-  int n_j; // final energy level (integer)
+  int Z; // Atomic number (integer)
+  int n_i; // Initial energy level (integer)
+  int n_j; // Final energy level (integer)
   char unit; // unit for output, either J (Joules) or e (Electron Volts) (character)
 
-  // We then ask the user to input values for Z, n_i, n_j, unit
-  std::cout << "\nPlease enter values for: Atomic number, initial quantum number, final quantum number, and unit (J or e):"; // Use the output stream std::cout to print a message to ask the user
-  std::cout << "\nEnter these values, seperated by spaces, in the order they appear above: "; // Prompt user on correct input format
-  
-  // We can then read values input via the users keyboard
-  // However, we must now check the formatting and mathematical validity of the user's input
-  // We can use a loop to prompt the user for new inputs until their input is totally valid
+  char restart = 'y'; // Gives user the option to restart calculation
 
-  bool valid_input = false; // Declare and initialise a boolean variable which tracks validity of user input
-  
-  // Create a loop that prompts new input until all validity checks are passed
-  while (!valid_input) {
-     
+  while (restart == 'y') {
+
+    // We then ask the user to input values for Z, n_i, n_j, unit
+    std::cout << "\nPlease enter values for: Atomic number, initial quantum number, final quantum number, and unit (J or e):"; // Use the output stream std::cout to print a message to ask the user
+    std::cout << "\nEnter these values, seperated by spaces, in the order they appear above: "; // Prompt user on correct input format
+
+    // We can then read values input via the users keyboard
+    // However, we must now check the formatting and mathematical validity of the user's input
+    // We can use a loop to prompt the user for new inputs until their input is totally valid
+
+    bool valid_input = false; // Declare and initialise a boolean variable which tracks validity of user input
+
+    // Create a loop that prompts new input until all validity checks are passed
+    while (!valid_input) {
+        
     std::cin >> Z >> n_i >> n_j >> unit; // Use the input stream std::cin to read user input
 
     // Check the validity of the input formatting
     if (std::cin.fail()) {
-      std::cout << "Invalid input format. Refer above to the input instructions, and try again: ";
-      std::cin.clear(); // Clear the error bit
-      std::cin.ignore(10000, '\n'); // Ignore invalid input
-      continue;
+        std::cout << "Invalid input format. Refer above to the input instructions, and try again: ";
+        std::cin.clear(); // Clear the error bit
+        std::cin.ignore(10000, '\n'); // Ignore invalid input
+        continue;
     }
 
     // Check the validity of the atomic number 
     else if (Z < 1) {
-      std::cout << "Invalid atomic number (must be a positive integer). Please try again: ";
+        std::cout << "Invalid atomic number (must be a positive integer). Please try again: ";
     }
 
     // Check the validity of the quantum numbers
     else if (n_i < 1 || n_j < 1) {
-      std::cout << "Invalid initial/final quantum numbers (must be positive integers). Please try again: ";
+        std::cout << "Invalid initial/final quantum numbers (must be positive integers). Please try again: ";
     }
 
     else if (n_i < n_j) { // CHECK IF THIS CAN BE <= OR JUST < WITH LECTURERS
-      std::cout << "Invalid initial/final quantum numbers (initial must be greater than final). Please try again: ";
+        std::cout << "Invalid initial/final quantum numbers (initial must be greater than final). Please try again: ";
     }
 
     // Check the validity of the units
     else if (unit != 'J' && unit != 'e') {
-      std::cout << "Invalid unit (must be J or e). Please try again: ";
+        std::cout << "Invalid unit (must be J or e). Please try again: ";
     }
 
     // End loop if all checks passed
     else {
-      valid_input = true;
+        valid_input = true;
     }
 
-  }
+    }
 
-  double E = formula(Z, n_i, n_j, unit); // Call function defined earlier with user input values
+    double E = formula(Z, n_i, n_j, unit); // Call function defined earlier with user input values
 
-  std::cout << "Transition energy = " << E; // Output result
+    std::cout << "Transition energy = " << E; // Output result
 
-  // Make sure we output result with correct units
-  if (unit == 'e') {
+    // Make sure we output result with correct units
+    if (unit == 'e') {
     std::cout << " eV\n";
-  }
-  
-  else {
+    }
+
+    else {
     std::cout << " J\n";
-  }
+    }
+
+    std::cout << "Would you like to calculate another value? Please enter (y/n): ";
+    std::cin >> restart;
+
+    while (restart != 'y' && restart != 'n') {
+    std::cout << "Invalid input. Please enter only (y/n): ";
+    std::cin >> restart;
+    }
+
+    }
 
   return 0;
 }
